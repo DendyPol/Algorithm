36cf76a49a919ae7df6b4688616843669f144557
DendyPol <Polovinko_91@icloud.com>
2025-04-26 15:55:23 +0300

    test commit main

--- src/main/java/ru/polovinko/ConcatInteger.java
+++ src/main/java/ru/polovinko/ConcatInteger.java
@@ -2,6 +2,8 @@ package ru.polovinko;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConcatInteger {
     public static void main(String[] args) {
@@ -10,8 +12,17 @@ public class ConcatInteger {
         lists.add(List.of(3, 4));
         lists.add(List.of(5, 6));
 
-        List<Integer> concat = new ArrayList<>();
+        System.out.println("Using loop: ");
+        List<Integer> testLoop = concatUsingLoop(lists);
+        System.out.println(testLoop);
 
+        System.out.println("Using stream: ");
+        List<Integer> testStream = concatUsingStream(lists);
+        System.out.println(testStream);
+    }
+
+    public static List<Integer> concatUsingLoop(List<List<Integer>> lists) {
+        List<Integer> concat = new ArrayList<>();
         for (int i = 0; i < lists.size() - 1; i++) {
             List<Integer> current = lists.get(i);
             List<Integer> next = lists.get(i + 1);
@@ -21,6 +32,17 @@ public class ConcatInteger {
                 }
             }
         }
-        System.out.println(concat);
+        return concat;
+    }
+
+    public static List<Integer> concatUsingStream(List<List<Integer>> lists) {
+        return IntStream.range(0, lists.size() - 1)
+                .boxed()
+                .flatMap(i -> lists.get(i).stream()
+                        .flatMap(a -> lists.get(i + 1).stream()
+                                .map(b -> Integer.parseInt(a.toString() + b.toString()))
+                        )
+                )
+                .collect(Collectors.toList());
     }
 }
